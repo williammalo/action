60196c4727e5703ee06a802a4819dd2093a1d596
william malo <williammalo2@gmail.com>
2012-10-26 10:14:24 -0400

    Merged source files with site

--- en/mail.php
+++ en/mail.php
@@ -1,20 +1,8 @@
-<!DOCTYPE html>
-<meta charset="utf-8">
-<title>ACTION UTILITY</title>
-
-<style>
-body{text-align:center}
-h1{display:block;color:#333}
-</style>
-<body>
-
 <?php
+header("Access-Control-Allow-Origin: *");
 $subject = "Email form:".$_POST["subject"];
 $message = $_POST["message"];
 $headers = "From:".$_POST["sender"];
 
-if($message!=""){mail("info@actionutilityquebec.com",$subject,$message,$headers);}
-echo "<br><h1>Mail sent.</h1>";
+if($message!=""){mail("williammalo2@gmail.com",$subject,$message,$headers);}
 ?>
-
-<a style=color:#07d href=contact>Back</a>
