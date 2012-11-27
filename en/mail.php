108352bde646293bc5adfb1bed23dac02b389d36
william malo <williammalo2@gmail.com>
2012-11-27 08:15:35 -0500

    the french and english site now share the same ressourse files / converted header to jpg for 3x less size

--- en/mail.php
+++
@@ -1,8 +0,0 @@
-<?php
-header("Access-Control-Allow-Origin: *");
-$subject = "Email form:".$_POST["subject"];
-$message = $_POST["message"];
-$headers = "From:".$_POST["sender"];
-
-if($message!=""){mail("williammalo2@gmail.com",$subject,$message,$headers);}
-?>
